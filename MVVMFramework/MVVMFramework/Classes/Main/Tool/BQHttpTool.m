feb666f6405ff7f2e021a5e15cee7d4abd7e7349
momo <928627819@qq.com>
2015-12-27 17:20:29 +0800

    对代码进行了一些优化和改进

--- MVVMFramework/MVVMFramework/Classes/Main/Tool/BQHttpTool.m
+++ MVVMFramework/MVVMFramework/Classes/Main/Tool/BQHttpTool.m
@@ -18,16 +18,23 @@
     // 设置请求ContentType
     // self.acceptableContentTypes = [NSSet setWithObjects:@"application/json", @"text/json", @"text/javascript", nil];
     mgr.responseSerializer.acceptableContentTypes = [NSSet setWithObject:@"text/html"];
-    // 2.发送请求
-    [mgr GET:url parameters:params progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-        if (success) {
-            success(responseObject);
-        }
-    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-        if (failure) {
-            failure(error);
-        }
-    }];
+    
+    if ([AFNetworkReachabilityManager manager].isReachable) {
+        // 2.发送请求
+        [mgr GET:url parameters:params progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+            if (success) {
+                success(responseObject);
+            }
+        } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+            if (failure) {
+                failure(error);
+            }
+        }];
+        
+    } else {
+        [BQHttpTool showExceptionDialog];
+    }
+    
 }
 
 + (void)post:(NSString *)url params:(NSDictionary *)params success:(void (^)(id json))success failure:(void (^)(NSError *error))failure
@@ -37,16 +44,29 @@
     // 设置请求ContentType
     // self.acceptableContentTypes = [NSSet setWithObjects:@"application/json", @"text/json", @"text/javascript", nil];
     mgr.responseSerializer.acceptableContentTypes = [NSSet setWithObject:@"text/html"];
-    // 2.发送请求
-    [mgr POST:url parameters:params progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-        if (success) {
-            success(responseObject);
-        }
-    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-        if (failure) {
-            failure(error);
-        }
-    }];
-
+    if ([AFNetworkReachabilityManager manager].isReachable) {
+        // 2.发送请求
+        [mgr POST:url parameters:params progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+            if (success) {
+                success(responseObject);
+            }
+        } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+            if (failure) {
+                failure(error);
+            }
+        }];
+    } else {
+        [BQHttpTool showExceptionDialog];
+    }
+    
+}
+//弹出网络错误提示框
++ (void)showExceptionDialog
+{
+    [[[UIAlertView alloc] initWithTitle:@"提示"
+                                message:@"网络异常，请检查网络连接"
+                               delegate:self
+                      cancelButtonTitle:@"好的"
+                      otherButtonTitles:nil, nil] show];
 }
 @end
