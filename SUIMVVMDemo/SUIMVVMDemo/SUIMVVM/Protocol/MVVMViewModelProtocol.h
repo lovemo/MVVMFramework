cf2920ac70a74a347e6f4fbf9a6a4b1a1556adab
momo <928627819@qq.com>
2016-02-23 10:14:36 +0800

    add protocol

---
+++ SUIMVVMDemo/SUIMVVMDemo/SUIMVVM/Protocol/MVVMViewModelProtocol.h
@@ -0,0 +1,19 @@
+//
+//  MVVMViewModelProtocol.h
+//  SUIMVVMDemo
+//
+//  Created by yuantao on 16/2/23.
+//  Copyright © 2016年 lovemo. All rights reserved.
+//
+
+#ifndef MVVMViewModelProtocol_h
+#define MVVMViewModelProtocol_h
+
+@protocol MVVMViewModelProtocol <NSObject>
+
+@optional
+
+
+@end
+
+#endif /* MVVMViewModelProtocol_h */
