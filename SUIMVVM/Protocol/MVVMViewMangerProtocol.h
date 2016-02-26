f451e42a9e4cfc97a720b13f7890e05cc5b56cd9
momo <928627819@qq.com>
2016-02-26 09:10:12 +0800

    add protocol

--- SUIMVVM/Protocol/MVVMViewMangerProtocol.h
+++ SUIMVVM/Protocol/MVVMViewMangerProtocol.h
@@ -13,6 +13,27 @@
 
 @optional
 
+/**
+ *  设置Controller的子视图的管理者为self
+ *
+ *  @param superView 一般指subView所在控制器的view
+ */
+- (void)handleViewMangerWithSuperView:(UIView *)superView;
+
+/**
+ *  设置subView的管理者为self
+ *
+ *  @param subView 管理的subView
+ */
+- (void)handleViewMangerWithSubView:(UIView *)subView;
+
+/**
+ *  设置添加subView的事件
+ *
+ *  @param view 管理的subView
+ *  @param info 附带信息，用于区分调用
+ */
+- (void)handleViewMangerActionWithView:(UIView *)view info:(NSString *)info;
 
 @end
 
