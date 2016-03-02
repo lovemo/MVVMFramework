46ff1e228a3db884db83b7c5b92da2ac85820b7b
momo <928627819@qq.com>
2016-03-02 17:29:33 +0800

    update pods

--- SUIMVVMKit/SUIMVVMKit.h
+++ SUIMVVMKit/SUIMVVMKit.h
@@ -1,18 +1,18 @@
 //
-//  BQMVVM.h
+//  SUIMVVMKit.h
 //  MVVMFramework
 //
 //  Created by yuantao on 16/1/21.
 //  Copyright © 2016年 momo. All rights reserved.
 //
 
-#ifndef BQMVVM_h
-#define BQMVVM_h
+#ifndef SUIMVVMKit_h
+#define SUIMVVMKit_h
 
 #import "MVVMBaseViewModel.h"
 #import "MVVMBaseViewManger.h"
 #import "MVVMTableDataDelegate.h"
-#import "MVVMCollectionDataDelegate.H"
+#import "MVVMCollectionDataDelegate.h"
 #import "MVVMExtend.h"
 #import "MVVMConstant.h"
 #import "MVVMSingleton.h"
@@ -20,4 +20,4 @@
 #import "UITableView+FDTemplateLayoutCell.h"
 #import "PMKVObserver.h"
 
-#endif /* BQMVVM_h */
+#endif /* SUIMVVMKit_h */
