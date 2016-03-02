9b96624c56685ecaea0c4348a5da6a6d1ae18c81
momo <928627819@qq.com>
2016-03-02 15:27:00 +0800

    update pods

--- SUIMVVMDemo/SUIMVVMDemo/SUIMVVMKit/SUIMVVMKit.h
+++ SUIMVVMDemo/SUIMVVMDemo/SUIMVVMKit/SUIMVVMKit.h
@@ -10,8 +10,14 @@
 #define BQMVVM_h
 
 #import "MVVMBaseViewModel.h"
+#import "MVVMBaseViewManger.h"
 #import "MVVMTableDataDelegate.h"
 #import "MVVMCollectionDataDelegate.H"
 #import "MVVMExtend.h"
+#import "MVVMConstant.h"
+#import "MVVMSingleton.h"
+#import "SUIUtils.h"
+#import "UITableView+FDTemplateLayoutCell.h"
+#import "PMKVObserver.h"
 
 #endif /* BQMVVM_h */
